bbf4b0c3e788b7ab4c7186882fae8cbebab713c0
Aure <aure.enkaoua@gmail.com>
2024-03-24 19:13:00 +0000

    feat, fix: adding pointer tracking to code

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # OPERATION AR
 
-To run:
+# 1) Clone the repository
 
-First alibrate camera and add to data/calibration as .txt file 
+# 2) Install the requirements
+
+```
+pip install -r requirements.txt
+```
+
+# 3) Run the code
+
+
+First calibrate camera and add to data/calibration as .txt file 
 
 Then run 
 
 ```
 python cl_main.py --config config/config.ini
-``
--- src/AR_gui_main_widget.py
+++ src/AR_gui_main_widget.py
@@ -114,7 +114,8 @@ class ARGuiMainWidget(bw.ARGuiBaseWidget):
         """
         Called by update_view in base class.
         """
-        pose_ok, annotated_image, pose = self.detect_aruco_board_pose(img_undistorted,
+        annotated_image = np.copy(img_undistorted)
+        pose_ok, annotated_image, pose = self.detect_aruco_board_pose(annotated_image,
                                                                                 img_undistorted_grey,
                                                                                 self.intrinsics, 
                                                                                 self.aruco_board,
@@ -126,34 +127,51 @@ class ARGuiMainWidget(bw.ARGuiBaseWidget):
                                                                                 self.pointer_aruco_board, 
                                                                                 self.pointer_aruco_dict)
         # First set video images.
-        if pose_ok:
-            self.video_viewer.set_video_image(annotated_image)
-        else:
-            self.video_viewer.set_video_image(img_undistorted)
+        self.video_viewer.set_video_image(annotated_image)
+            #self.video_viewer.set_video_image(img_undistorted)
 
         # Then sort out cameras. Slight code duplication, but easier to read.
         # So, currently, if tracking not ok, then overlays stop updating.
+        """
+        if pose_ok:
+            self.video_viewer.set_camera_matrix(self.intrinsics)
+            self.video_viewer.set_camera_pose(self.registration_matrix)
+
+            min_clip, max_clip = guess_clipping_range_from_pose(np.eye(4))
+            self.video_viewer.get_foreground_camera().SetClippingRange(min_clip, max_clip)
+
+            for m in self.model_loader.models:
+                if m.get_name() in ['pointer']:
+                    if pointer_pose_ok:
+                        print('found pointer', m.get_name())
+                        m.set_user_matrix(mu.create_vtk_matrix_from_numpy(np.linalg.inv(pose_pointer)))
+                else:
+                    m.set_user_matrix(mu.create_vtk_matrix_from_numpy(np.linalg.inv(pose)))
+        """
+
         if pose_ok:
             # ArUco pose is aruco_board (world) to camera.
             # The VTKOverlayWindow expects camera to world.
             # So, we have to invert the pose.
 
             camera_to_world = np.linalg.inv(pose @ self.registration_matrix)
-            world_to_pointer = pose_pointer @ np.linalg.inv(camera_to_world)
-            # hand_T_aruco = pose
-
+            # set camera pose relative to world
             self.video_viewer.set_camera_matrix(self.intrinsics)
             self.video_viewer.set_camera_pose(camera_to_world)
-
-            world_mtx_vtk = mu.create_vtk_matrix_from_numpy(np.linalg.inv(pose))
-            pointer_mtx_vtk = mu.create_vtk_matrix_from_numpy(world_to_pointer)
-            for m in self.model_loader.models:
-                if m.get_name() in ['pointer', 'weiss']:
-                    print('found model')
-                    m.set_model_transform(pointer_mtx_vtk)
-                #else:
-                    #m.set_user_matrix(world_mtx_vtk)
-                        
-            min_clip, max_clip = guess_clipping_range_from_pose(world_to_pointer)
+            min_clip, max_clip = guess_clipping_range_from_pose(camera_to_world)
             self.video_viewer.get_foreground_camera().SetClippingRange(min_clip, max_clip)
+
+            #world_mtx_vtk = mu.create_vtk_matrix_from_numpy(np.linalg.inv(pose))
+
+            # set pointer model relative to world
+            if pointer_pose_ok:
+                # to get the pointer relative to the world reference: pointer to camera multiplied by camera to world
+                world_to_pointer = camera_to_world@pose_pointer
+                pointer_mtx_vtk = mu.create_vtk_matrix_from_numpy(world_to_pointer)
+                # move the pointer model to the pointer position
+                for m in self.model_loader.models:
+                    if m.get_name() in ['tweezers']:
+                        print('found pointer', m.get_name())
+                        m.set_user_matrix(pointer_mtx_vtk)
+
         self.video_viewer.Render()
